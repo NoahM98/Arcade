0dc33f075f38db6156068a3c81ae1c06be601620
Noah Mauer <noahmauer1998@gmail.com>
2023-04-27 23:20:25 -0400

    Added renderState function to update the visuals on the board

--- tic-tac-toe/app.js
+++ tic-tac-toe/app.js
@@ -1,1 +1,42 @@
-console.log("hello world");
+// state
+let state = {
+    player1: 'x',
+    player2: 'o',
+    board: [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+    ]
+}
+
+let currentPlayer = state.player1;
+
+function buildInitialState() {
+    // displays player name and displays whether its one player or two player
+}
+
+// render
+function renderState() {
+    const cellValue = document.createElement('div');
+    cellValue.innerText = currentPlayer;
+    const clickedCell = document.getElementById(target.id);
+    clickedCell.appendChild(cellValue);
+}
+
+function updateBoard() {
+
+}
+
+// maybe a dozen or so helper functions for tiny pieces of the interface
+
+// listeners
+function onBoardClick(event) {
+    // update state, maybe with another dozen or so helper functions...
+    target = event.target;
+    if (target.matches('.cell') && target.innerText === '') {
+        updateBoard();
+        renderState(); // show the user the new state
+    }
+}
+const board = document.getElementById('game-board');
+board.addEventListener('click', onBoardClick); // etc
