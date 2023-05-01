15fd5ae41cd510fa7b4add2ddae735c2eb471d82
Noah Mauer <noahmauer1998@gmail.com>
2023-04-30 22:46:56 -0400

    Added checkWinConditions() to evaluate whether the game is over

--- tic-tac-toe/app.js
+++ tic-tac-toe/app.js
@@ -1,4 +1,3 @@
-// state
 let state = {
     player1: 'x',
     player2: 'o',
@@ -12,27 +11,48 @@ let state = {
 let currentPlayer = state.player1;
 
 function buildInitialState() {
-    // displays player name, displays whether its one player or two player, and checks whether the game is over (someone won or it was a draw)
+    // displays player name and displays whether its one player or two player
+
 }
 
-// render
 function renderState() {
     const cellValue = document.createElement('div');
     cellValue.innerText = currentPlayer;
     const clickedCell = document.getElementById(target.id);
     clickedCell.appendChild(cellValue);
-    if (currentPlayer === state.player1) {
-        currentPlayer = state.player2;
-    } else if (currentPlayer === state.player2) {
-        currentPlayer = state.player1;
-    }
 }
 
 function updateBoard() {
     state.board[target.id[5]].splice(target.id[6], 1, currentPlayer);
 }
 
-// maybe a dozen or so helper functions for tiny pieces of the interface
+function checkWinConditions() {
+    let board = state.board;
+    let draw;
+    let notDraw;
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+            if (board[i][j] !== null) {
+                draw = true;
+            } else {
+                notDraw = true;
+            }
+        }
+    }
+    if ((board[0][0] === board[0][1] && board[0][0] === board[0][2] && board[0][0] !== null) ||
+        (board[1][0] === board[1][1] && board[1][0] === board[1][2] && board[1][0] !== null) ||
+        (board[2][0] === board[2][1] && board[2][0] === board[2][2] && board[2][0] !== null) ||
+        (board[0][0] === board[1][0] && board[0][0] === board[2][0] && board[0][0] !== null) ||
+        (board[0][1] === board[1][1] && board[0][1] === board[2][1] && board[0][1] !== null) ||
+        (board[0][2] === board[1][2] && board[0][2] === board[2][2] && board[0][2] !== null) ||
+        (board[0][0] === board[1][1] && board[0][0] === board[2][2] && board[0][0] !== null) ||
+        (board[0][2] === board[1][1] && board[0][2] === board[2][0] && board[0][2] !== null)) {
+
+        console.log(`Player ${currentPlayer} wins!`);
+    } else if (draw && !notDraw) {
+        console.log("It's a draw!");
+    }
+}
 
 // listeners
 function onBoardClick(event) {
@@ -40,8 +60,14 @@ function onBoardClick(event) {
     target = event.target;
     if (target.matches('.cell') && target.innerText === '') {
         buildInitialState();
+        renderState();
         updateBoard();
-        renderState(); // show the user the new state
+        checkWinConditions();
+        if (currentPlayer === state.player1) {
+            currentPlayer = state.player2;
+        } else if (currentPlayer === state.player2) {
+            currentPlayer = state.player1;
+        }
     }
 }
 const board = document.getElementById('game-board');
