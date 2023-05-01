5e89e0e8fb2cfa6fa64a8fc1c965b2ca4b9204e6
Noah Mauer <noahmauer1998@gmail.com>
2023-04-30 21:51:02 -0400

    Added updateBoard() so state.board will match the visual board

--- tic-tac-toe/app.js
+++ tic-tac-toe/app.js
@@ -12,7 +12,7 @@ let state = {
 let currentPlayer = state.player1;
 
 function buildInitialState() {
-    // displays player name and displays whether its one player or two player
+    // displays player name, displays whether its one player or two player, and checks whether the game is over (someone won or it was a draw)
 }
 
 // render
@@ -21,10 +21,15 @@ function renderState() {
     cellValue.innerText = currentPlayer;
     const clickedCell = document.getElementById(target.id);
     clickedCell.appendChild(cellValue);
+    if (currentPlayer === state.player1) {
+        currentPlayer = state.player2;
+    } else if (currentPlayer === state.player2) {
+        currentPlayer = state.player1;
+    }
 }
 
 function updateBoard() {
-
+    state.board[target.id[5]].splice(target.id[6], 1, currentPlayer);
 }
 
 // maybe a dozen or so helper functions for tiny pieces of the interface
@@ -34,6 +39,7 @@ function onBoardClick(event) {
     // update state, maybe with another dozen or so helper functions...
     target = event.target;
     if (target.matches('.cell') && target.innerText === '') {
+        buildInitialState();
         updateBoard();
         renderState(); // show the user the new state
     }
